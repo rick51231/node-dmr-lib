bc46b675e06c9c96fa54f2c5b634a67a7f8cfa40
rick51231 <rick51231@users.noreply.github.com>
2022-10-09 23:13:49 +0300

    Data transport: HomeBrew and IPv4.

---
+++ src/transport/IP4.js
@@ -0,0 +1,91 @@
+class IP4 {
+    protocol = 0;
+    identification = 0;
+
+    src_addr = 0;
+    dst_addr = 0;
+
+    src_port = 0;
+    dst_port = 0;
+
+    payload = Buffer.alloc(0);
+
+
+    static from(buffer) {
+        let packet = new IP4();
+
+        if(IP4.getIPChecksum(buffer)!==0) //Invalid IP checksum
+            return undefined;
+
+        packet.protocol = buffer.readUInt8(9);
+        packet.identification = buffer.readUInt16BE(4);
+
+        packet.src_addr = buffer.readUInt32BE(12);
+        packet.dst_addr = buffer.readUInt32BE(16);
+
+        if(packet.protocol === 0x17) {
+            if(IP4.getUDPChecksum(buffer)!==0)
+                return undefined;
+
+            packet.src_port = buffer.readUInt16BE(20);
+            packet.dst_port = buffer.readUInt16BE(22);
+
+            let udpLen = buffer.readUInt16BE(24);
+
+            packet.payload = buffer.slice(28, 28+udpLen-8); //8 = udp header size
+        }
+
+        return packet;
+    }
+
+    static getIPChecksum(buffer) {
+        let headerChk = 0x00;
+        for(let i = 0; i<10; i++) {
+            headerChk += buffer.readUInt16BE(i*2);
+        }
+
+        while (headerChk>>16) {
+            headerChk = (headerChk & 0xffff) + (headerChk >> 16);
+        }
+
+        headerChk = 0xffff - headerChk;
+
+        return headerChk;
+    }
+
+    static getUDPChecksum(buffer) {
+        let ipHeaderSize = 20; //5 = 20 ???
+
+        let udpSize = buffer.readUInt16BE(24);
+
+        if(udpSize+ipHeaderSize > buffer.length)
+            udpSize = buffer.length - ipHeaderSize;
+
+        let udpChk = 0x00;
+
+        udpChk += buffer.readUInt32BE(12) & 0xFFFF; //(this.src_addr>>16)&0xFFFF;
+        udpChk += (buffer.readUInt32BE(12) >> 16) & 0xFFFF; //(this.src_addr)&0xFFFF;
+        udpChk += buffer.readUInt32BE(16) & 0xFFFF; //(this.dst_addr>>16)&0xFFFF;
+        udpChk += (buffer.readUInt32BE(16) >> 16) & 0xFFFF; //(this.dst_addr)&0xFFFF;
+        udpChk += 17; //Protocol udp
+        udpChk += udpSize;
+
+        for(let i = 0; i<udpSize; i+=2) { //Todo: if size %2 == 1
+            if(i === udpSize-1 && udpSize % 2 === 1) {
+                udpChk += buffer.readUInt8(ipHeaderSize + i) << 8;
+            } else {
+                udpChk += buffer.readUInt16BE(ipHeaderSize + i);
+            }
+
+        }
+
+        while (udpChk>>16) {
+            udpChk = (udpChk & 0xffff) + (udpChk >> 16);
+        }
+        udpChk = 0xffff - udpChk;
+
+        return udpChk;
+    }
+}
+
+module.exports = IP4;
