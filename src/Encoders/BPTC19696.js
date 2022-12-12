f0eb9ee23329511533af3106544c942c433c14f2
rick51231 <rick51231@users.noreply.github.com>
2022-12-12 18:16:55 +0300

    BPTC19696 encoder.

--- src/Encoders/BPTC19696.js
+++ src/Encoders/BPTC19696.js
@@ -12,6 +12,14 @@ class BPTC19696 {
         return dataBits.map(i => i ? 1 : 0).join('');
     }
 
+    static encode(data) {
+        let extractedData = this.encodeExtractData(data);
+        let errorChecked = this.encodeErrorCheck(extractedData);
+        let interleaved = this.encodeInterleave(errorChecked);
+
+        return interleaved.join('');
+    }
+
     static decodeDeInterleave(bitArray) {
         let ret = [];
         bitArray[0] = true;
@@ -98,6 +106,90 @@ class BPTC19696 {
 
         for (let a = 121; a <= 131; a++, pos++)
         ret[pos] = bitArray[a];
+
+        return ret;
+    }
+
+    static encodeExtractData(bitArray) {
+        let ret = [];
+
+        for (let i = 0; i < 196; i++)
+            ret[i] = 0;
+
+        let pos = 0;
+
+        for (let a = 4; a <= 11; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        for (let a = 16; a <= 26; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        for (let a = 31; a <= 41; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        for (let a = 46; a <= 56; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        for (let a = 61; a <= 71; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        for (let a = 76; a <= 86; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        for (let a = 91; a <= 101; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        for (let a = 106; a <= 116; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        for (let a = 121; a <= 131; a++, pos++)
+            ret[a] = bitArray[pos];
+
+        return ret;
+    }
+
+    static encodeErrorCheck(bitArray) {
+        let ret = bitArray;
+        // Run through each of the 9 rows containing data
+        for (let r = 0; r < 9; r++) {
+            let pos = (r * 15) + 1;
+            ret = Hamming.encode15113_2(ret, pos);
+        }
+
+        // Run through each of the 15 columns
+        let col = Array(13);
+        for (let c = 0; c < 15; c++) {
+            let pos = c + 1;
+            for (let a = 0; a < 13; a++) {
+                col[a] = bitArray[pos];
+                pos = pos + 15;
+            }
+
+            col = Hamming.encode1393(col);
+
+            pos = c + 1;
+            for (let a = 0; a < 13; a++) {
+                ret[pos] = col[a];
+                pos = pos + 15;
+            }
+        }
+
+        return ret;
+    }
+
+    static encodeInterleave(bitArray) {
+        let ret = [];
+
+        for (let i = 0; i < 196; i++)
+            ret[i] = 0;
+
+        // The first bit is R(3) which is not used so can be ignored
+        for (let a = 0; a < 196; a++)	{
+            // Calculate the interleave sequence
+            let interleaveSequence = (a * 181) % 196;
+            // Unshuffle the data
+            ret[interleaveSequence] = bitArray[a];
+        }
 
         return ret;
     }
