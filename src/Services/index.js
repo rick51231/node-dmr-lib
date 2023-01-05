69f8248f58b256217593ae2e771bda5210bfb6f4
rick51231 <rick51231@users.noreply.github.com>
2023-01-05 15:01:08 +0300

    Exports fix.

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,4 +1,5 @@
 module.exports.DMRIPGateway = require('./DMRIPGateway');
 module.exports.DMRServices = require('./DMRServices');
+module.exports.HBDMRGateway = require('./HBDMRGateway');
 module.exports.IPSCPeer = require('./IPSCPeer');
 module.exports.Utils = require('./Utils');
