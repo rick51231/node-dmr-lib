9c1cd6e617b29f6a2706f62805135473f74d099c
rick51231 <rick51231@users.noreply.github.com>
2026-01-17 19:24:59 +0300

    Updated motorola network consts

--- src/Motorola/Network.js
+++ src/Motorola/Network.js
@@ -11,18 +11,28 @@ class Network {
     static PORTID_LRRP      = 0x01;
     static PORTID_ARS       = 0x03;
     static PORTID_TMS       = 0x04;
-    static PORTID_TELEMETRY = 0x05;
+    static PORTID_TLM       = 0x05;
 
     static PORTID_NEW_CONSTANT = 94; //New radios (aka DP4000) uses "DMR Standart" compression with this portId offset
 
+
+    //CPS help str: It is not recommended to use the port numbers reserved for internal applications (4001, 4004, 4005, 4007, 4008, 4061, 4062, 4063, 4066, 4067, 4068, 4069).
+    static PORT_DDMS      = 3000; // Presence Notifier (PN) service TCP
     static PORT_LRRP      = 4001; // Location
+    static PORT_UNK0      = 4004; // XCMP server ?
     static PORT_ARS       = 4005; // Registration
     static PORT_TMS       = 4007; // Text messages
     static PORT_TLM       = 4008; // Telemetry
+    static PORT_OTA       = 4009; // Over the air programming
+    static PORT_UNK1      = 4010; // Extended TMS ? / Possible some commands after OTA
+    static PORT_UNK2      = 4011; // ?
     static PORT_BMS       = 4012; // Battery management
-    static PORT_JMS       = 4013; // Job tickets ?
-    static PORT_LIP       = 5017; // Indoor location ?
+    static PORT_JMS       = 4013; // Job tickets
+    static PORT_SRR       = 4015; // Sensor Request Response
+    static PORT_ATT       = 4064; // AirTracer
+    static PORT_LIP       = 5017; // Indoor location
     static PORT_XNL       = 8002; // XNL/XCMP TCP connection
+    static PORT_XNLS      = 8003; // XNL/XCMP secure TCP connection
 
 
     static AddressID2Network(addressId) {
@@ -62,8 +72,8 @@ class Network {
                 return Network.PORT_ARS;
             case Network.PORTID_TMS:
                 return Network.PORT_TMS;
-            case Network.PORTID_TELEMETRY:
-                return Network.PORT_TELEMETRY;
+            case Network.PORTID_TLM:
+                return Network.PORT_TLM;
         }
 
         return null;
@@ -75,14 +85,14 @@ class Network {
             case Network.PORT_LRRP:
                 portId = Network.PORTID_LRRP;
                 break;
-            case Network.PORTID_ARS:
+            case Network.PORT_ARS:
                 portId = Network.PORTID_ARS;
                 break;
-            case Network.PORTID_TMS:
+            case Network.PORT_TMS:
                 portId = Network.PORTID_TMS;
                 break;
-            case Network.PORTID_TELEMETRY:
-                portId = Network.PORTID_TELEMETRY;
+            case Network.PORT_TLM:
+                portId = Network.PORTID_TLM;
                 break;
             default:
                 return null;
